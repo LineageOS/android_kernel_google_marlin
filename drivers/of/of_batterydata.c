9ebb4e71b2e69cfd816076800fed4556f1f33556
Derek <nielson925@gmail.com>
2023-05-08 04:14:52 -0600

    of_batterydata: Update htc of_batterydata_get_best_profile

--- drivers/of/of_batterydata.c
+++ drivers/of/of_batterydata.c
@@ -313,26 +313,12 @@ static int64_t of_batterydata_convert_battery_id_kohm(int batt_id_uv,
 #ifdef CONFIG_HTC_BATT
 struct device_node *of_batterydata_get_best_profile(
 		const struct device_node *batterydata_container_node,
-		const char *psy_name,  const char  *batt_type)
+		int batt_id_kohm, const char *batt_type)
 {
 	struct device_node *cur_node = NULL;
-	struct power_supply *psy = NULL;
-	union power_supply_propval ret = {0, };
 	int rc = 0, id_raw_min = 0, id_raw_max = 0, htc_batt_id_ohm = 0;
 
-	psy = power_supply_get_by_name(psy_name);
-	if (!psy) {
-		pr_err("%s supply not found. defer\n", psy_name);
-		return ERR_PTR(-EPROBE_DEFER);
-	}
-
-	rc = psy->get_property(psy, POWER_SUPPLY_PROP_RESISTANCE_ID, &ret);
-	if (rc) {
-		pr_err("failed to retrieve resistance value rc=%d\n", rc);
-		return ERR_PTR(-ENOSYS);
-	}
-
-	htc_batt_id_ohm = ret.intval;
+	htc_batt_id_ohm = batt_id_kohm * 1000;
 
 	for_each_child_of_node(batterydata_container_node, cur_node) {
 		rc = of_property_read_u32(cur_node, "htc,id_raw_min", &id_raw_min);
